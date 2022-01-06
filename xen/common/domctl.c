fa874551471c39512acea47b81aba37bd3201db6
oftyFonyuy <fonyuy.caleb@ofty.fr>
2022-01-06 13:22:03 +0100

    logging issues

--- xen/common/domctl.c
+++ xen/common/domctl.c
@@ -287,9 +287,6 @@ long do_domctl(XEN_GUEST_HANDLE_PARAM(xen_domctl_t) u_domctl)
     if ( op->interface_version != XEN_DOMCTL_INTERFACE_VERSION )
         return -EACCES;
 
-    /* Migration log stub */
-    printk(XENLOG_G_DEBUG
-            "Calling do_domctl with opt->cmd =  %d\n", op->cmd);
     switch ( op->cmd )
     {
     case XEN_DOMCTL_assign_device:
